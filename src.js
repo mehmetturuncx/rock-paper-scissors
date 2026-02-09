71bc949aa0a42ea743fd905278146fc163765734
mehmetturuncx <mehmetturuncx@gmail.com>
2026-02-09 23:40:26 +0300

    Changed game model console based to UI based. Added buttons and writed the scores with DOM to the screen.

--- src.js
+++ src.js
@@ -11,78 +11,6 @@ function getComputerChoice(){
         return "Scissors"
     }
 }
-<<<<<<< HEAD
-
-function getHumanChoice(){
-    let chioce = prompt("Rock-Paper-Scissors\nChoose your item: ").toLowerCase()
-
-    if(chioce === "rock"){
-        return "Rock"
-    }
-    else if(chioce === "paper"){
-        return "Paper"
-    }
-    else if(chioce === "scissors"){
-        return "Scissors"
-    }
-}
-
-let humanScore = 0, computerScore = 0
-
-function playRound(humanChoice,computerChoice){
-    if(humanChoice === computerChoice){
-        console.log("Tie!")
-        humanScore++
-        computerScore++
-    }
-    else if(humanChoice === "Rock" && computerChoice === "Paper"){
-        console.log("Computer wins!")
-        computerScore++
-    }
-    else if(humanChoice === "Rock" && computerChoice === "Scissors"){
-        console.log("Human wins!")
-        humanScore++
-    }
-    else if(humanChoice === "Paper" && computerChoice === "Rock"){
-        console.log("Human wins!")
-        humanScore++   
-    }
-    else if(humanChoice === "Paper" && computerChoice === "Scissors"){
-        console.log("Computer wins!")
-        computerScore++
-    }
-    else if(humanChoice === "Scissors" && computerChoice === "Paper"){
-        console.log("Human wins!")
-        humanScore++
-    }
-    else if(humanChoice === "Scissors" && computerChoice === "Rock"){
-        console.log("Computer wins!")
-        computerScore++
-    }
-}
-
-while(true){
-
-    const humanSelection = getHumanChoice()
-    const computerSelection = getComputerChoice()
-
-    playRound(humanSelection, computerSelection)
-
-    if(humanScore === 5){
-        console.log("Game over, human wins!")
-        break
-    }
-    else if(computerScore === 5){
-        console.log("Game over, computer wins!")
-        break
-    }
-    else if(humanScore === 5 && computerScore === 5){
-        console.log("Game over, tie!")
-        break
-    }
-}
-
-=======
 const resultDiv = document.getElementById("results")
 const scoreDiv = document.getElementById("score")
 const winnerDiv = document.getElementById("winner")
@@ -137,5 +65,4 @@ function disableButtons(){
     paperButton.disabled =  true
     scissorsButton.disabled =  true
 }
->>>>>>> rps-ui
 
